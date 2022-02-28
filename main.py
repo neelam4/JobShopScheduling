72390b71e421473b5deef869d85b39f890adaeb6
Vikash <vikash.kumar.186057@gmail.com>
2022-02-28 15:36:01 +0530

    main is changed to original

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from ion import Job
+from service import JobSchedulerServiceImpl
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    print("JOB SCHEDULER")
+    print()
+    jobScheduler = JobSchedulerServiceImpl.JobSchedulerServiceImpl()
+    job1 = Job.Job("J1", 10, 0, 10, 1)
+    job2 = Job.Job("J2", 20, 0, 40, 1)
+    job3 = Job.Job("J3", 15, 2, 40, 2)
+    job4 = Job.Job("J4", 30, 1, 40, 2)
+    job5 = Job.Job("J5", 10, 2, 30, 2)
+    job6 = Job.Job("J6", 30, 2, 30, 2)
+    jobs = list()
+    jobs.append(job1)
+    jobs.append(job2)
+    jobs.append(job3)
+    jobs.append(job4)
+    jobs.append(job5)
+    jobs.append(job6)
+    jobScheduler.firstComeFirstServe(2, jobs)
+    print()
+    jobScheduler.shortestJobFirst(2, jobs)
+    print()
+    jobScheduler.fixedPriorityScheduling(2, jobs)
+    print()
+    jobScheduler.earliestDeadlineFirst(2, jobs)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
